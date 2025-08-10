3bd8243e0227f350855f765dc31fe08abb7dfa9e
Owen Doherty <o.n.doherty@gmail.com>
2025-08-09 19:24:42 -0700

    Reduce corner bracket size to half for more subtle framing

--- src/app/waitlist/page.tsx
+++ src/app/waitlist/page.tsx
@@ -367,13 +367,13 @@ export default function WaitlistPage() {
               {/* Corner brackets */}
               <div className="absolute inset-0 pointer-events-none">
                 {/* Top-left bracket */}
-                <div className="absolute top-0 left-0 w-6 h-6 border-l-2 border-t-2 border-dark-brown"></div>
+                <div className="absolute top-0 left-0 w-3 h-3 border-l-2 border-t-2 border-dark-brown"></div>
                 {/* Top-right bracket */}
-                <div className="absolute top-0 right-0 w-6 h-6 border-r-2 border-t-2 border-dark-brown"></div>
+                <div className="absolute top-0 right-0 w-3 h-3 border-r-2 border-t-2 border-dark-brown"></div>
                 {/* Bottom-left bracket */}
-                <div className="absolute bottom-0 left-0 w-6 h-6 border-l-2 border-b-2 border-dark-brown"></div>
+                <div className="absolute bottom-0 left-0 w-3 h-3 border-l-2 border-b-2 border-dark-brown"></div>
                 {/* Bottom-right bracket */}
-                <div className="absolute bottom-0 right-0 w-6 h-6 border-r-2 border-b-2 border-dark-brown"></div>
+                <div className="absolute bottom-0 right-0 w-3 h-3 border-r-2 border-b-2 border-dark-brown"></div>
               </div>
               
               <span 
